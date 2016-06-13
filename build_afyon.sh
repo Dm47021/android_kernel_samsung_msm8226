b4068af23bb12dae02621b809af0f99ed9db2775
derek <dm47021@hotmail.com>
2016-06-13 12:36:10 -0400

    My Source Ver2, Fix Camera, Fix gcc 4.8 build, updates

--- build_afyon.sh
+++ build_afyon.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 
+# Build Variables
+export version=ZERO_Cyanogenmod
+export TOOLCHAIN=/home/dm47021/Android/toolchains/arm-eabi-4.8/bin/arm-eabi-
+
+export CPU_CORES=8
+
 # Colorize and add text parameters
 red=$(tput setaf 1) # red
 grn=$(tput setaf 2) # green
@@ -16,32 +22,100 @@ echo -e "${bldred}
      Cleaning Build Directory 
 ${txtrst}"
 
+rm -rf ./$version-DM47021.zip
+rm -rf zfiles/packaging/system/lib/modules/pronto/wlan.ko
+
 make mrproper
+
+loc=~/.gnome2/nautilus-scripts/SignScripts/
+date=$(date +%Y%m%d-%H:%M:%S)
+
+# Check for a log directory in ~/ and create if its not there
+[ -d ~/logs ] || mkdir -p ~/logs
 
 echo""
 echo -e "${bldgrn} 
       Source is Sanitary 
 ${txtrst}"
-
-
-echo""
 echo -e "${bldblu} 
       Building ZERO Kernel By DM47021 From Source
 ${txtrst}" 
+echo -e "${bldblu} 
+      Removing Old .config files
+${txtrst}" 
+rm -rf .config
 
-make -j64 ARCH=arm CROSS_COMPILE=/home/dm47021/Android/toolchains/arm-eabi-4.7/bin/arm-eabi- msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8926-sec_afyonltetmo_defconfig
-make -j64 ARCH=arm CROSS_COMPILE=/home/dm47021/Android/toolchains/arm-eabi-4.7/bin/arm-eabi-
+echo -e "${bldblu} 
+      Setting up Zero_Configuration
+${txtrst}" 
+cp cm_afyonlte_defconfig .config
+
+make -j$CPU_CORES ARCH=arm CROSS_COMPILE=$TOOLCHAIN
 
 echo -e "${bldgrn} 
       Kernel Built Sucessfully!!
 ${txtrst}" 
 
-#Append dtb to zImage
+echo -e "${bldgrn} 
+      Kernel Modules can be found in zfiles/packaging/system/lib/modules
+${txtrst}"
+
+# Create dir struct
+mkdir zfiles/packaging/system/lib/modules
+mkdir zfiles/packaging/system/lib/modules/pronto
+
+find -name '*.ko' -exec cp -av {} zfiles/packaging/system/lib/modules/ \;
+cp zfiles/packaging/system/lib/modules/wlan.ko zfiles/packaging/system/lib/modules/pronto/wlan.ko
+rm -rf zfiles/packaging/system/lib/modules/wlan.ko
+
+# Build dt.img
 echo""
 echo -e "${bldcya} 
-      Appending .dtb Files to zImage 
-      The end result will be zImage-dtb  in arch/arm/boot 
+      Building new dt.img
+      The end result will be dt.img  in arch/arm/boot 
 ${txtrst}" 
+
+./bin/dtbTool -o arch/arm/boot/dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/
+
+# Copy Prequisites for making Boot.img
+cp arch/arm/boot/dt.img zfiles/
+cp arch/arm/boot/zImage zfiles/
+
+
+echo""
+echo -e "${bldcya} 
+      Building Boot.img
+${txtrst}" 
+
+cd zfiles
+./mkbootzip.pl ramdisk/cm12.1
+
+echo""
+echo -e "${bldcya} 
+      Boot.img built
+      Building Zipfile
+${txtrst}" 
+
+cd ../
+
+cp zfiles/zero-krnl.zip ./$version-DM47021.zip
+
+echo""
+echo -e "${bldcya} 
+    Flashable zip created 
+    zero-krnl.zip in the root of the build dir
+${txtrst}" 
+
+echo""
+echo -e "${bldred} 
+     Cleaning Up Working Dirs
+${txtrst}"
+
+# Remove dirs after build
+rm -rf zfiles/packaging/boot.img 
+rm -rf zfiles/zImage 
+rm -rf zfiles/zero-krnl.zip
+rm -rf zfiles/packaging/system/lib/modules
 
 # Wait for user input
 echo""
